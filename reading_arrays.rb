0d5b4e1f462ad096ac5f3b6b39a6a35281bbd5bc
frisaac <frisaac@stu.naperville203.org>
2018-08-08 15:21:58 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -39,6 +39,6 @@ puts last_student_by_method
 
 def first_second_and_third_students
   # Write a solution that returns the first, second and third students
-  STUDENT_NAMES[0,2]
+  STUDENT_NAMES[0][2]
 end
 puts first_second_and_third_students
