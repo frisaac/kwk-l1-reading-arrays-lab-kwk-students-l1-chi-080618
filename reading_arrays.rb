2f4edffba3a9b8daf10f3539f741c32325c5dda4
frisaac <frisaac@stu.naperville203.org>
2018-08-08 15:21:19 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -15,30 +15,30 @@ puts first_student_by_index
 
 def fourth_student_by_index
   # Write a solution that returns the first student in the array student_names
-  puts STUDENT_NAMES[3]
+  STUDENT_NAMES[3]
 end
-fourth_student_by_index
+puts fourth_student_by_index
 
 def last_student_by_index
   # Write a solution that returns the first student in the array student_names
-  puts STUDENT_NAMES[5]
+  STUDENT_NAMES[5]
 end
-last_student_by_index
+puts last_student_by_index
 
 def first_student_by_method
   # Write a solution that returns the first student using the built in .first method
-  puts STUDENT_NAMES.first
+  STUDENT_NAMES.first
 end
-first_student_by_method
+puts first_student_by_method
 
 def last_student_by_method
   # Write a solution that returns the first student using the built in .last method
-  puts STUDENT_NAMES.last
+  STUDENT_NAMES.last
 end
-last_student_by_method
+puts last_student_by_method
 
 def first_second_and_third_students
   # Write a solution that returns the first, second and third students
-  puts STUDENT_NAMES[0,2]
+  STUDENT_NAMES[0,2]
 end
-first_second_and_third_students
+puts first_second_and_third_students
